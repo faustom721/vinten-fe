b1b4d1ea814b9ba48fb6bfa9638a117f80db0e71
Fausto Márquez <fausto_7m@hotmail.com>
2021-06-06 19:59:23 -0300

    Experimenting with theme and detecting system dark mode for theme change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import * as React from 'react';
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
 import jsonServerProvider from 'ra-data-json-server';
+
 import authProvider from './authProvider';
 import Dashboard from './screens/Dashboard';
+import { lightTheme, darkTheme } from './muiTheme';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-const App = () => (
-  <Admin
-    dashboard={Dashboard}
-    dataProvider={dataProvider}
-    authProvider={authProvider}
-  >
-    <Resource name='users' list={ListGuesser} />
-  </Admin>
-);
+const App = () => {
+  const isDarkModeEnabled = useMediaQuery('(prefers-color-scheme: dark)');
+
+  return (
+    <Admin
+      theme={isDarkModeEnabled ? darkTheme : lightTheme}
+      dashboard={Dashboard}
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+    >
+      <Resource name='users' list={ListGuesser} />
+    </Admin>
+  );
+};
 
 export default App;
---
+++ src/muiTheme.js
@@ -0,0 +1,48 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import red from '@material-ui/core/colors/red';
+
+const commonStyles = {
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+  },
+  overrides: {
+    MuiButton: {
+      // override the styles of all instances of this component
+      root: {
+        // Name of the rule
+        color: 'white', // Some CSS
+      },
+    },
+  },
+};
+
+const lightTheme = createMuiTheme({
+  ...commonStyles,
+
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
+
+const darkTheme = createMuiTheme({
+  ...commonStyles,
+
+  palette: {
+    type: 'dark', // Switching the dark mode on is a single property value change.
+  },
+});
+
+export { lightTheme, darkTheme };
