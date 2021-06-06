7f9751a73901db95ba5db0230d644383bed6dcf0
Fausto Márquez <fausto_7m@hotmail.com>
2021-06-06 17:41:08 -0300

    login with authProvider is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
 import Dashboard from './screens/Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin
+    dashboard={Dashboard}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+  >
     <Resource name='users' list={ListGuesser} />
   </Admin>
 );
